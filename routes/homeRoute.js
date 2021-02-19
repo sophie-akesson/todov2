6b6b27e012056bb61bfa830162fdf20ec34ed976
Sophie Åkesson <sophie.akesson@gmail.com>
2021-02-19 17:58:03 +0100

    Add complete to do feature

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { homeRender, logoutRender } = require("../controllers/homeController");
-const { newToDoSubmit, removeSubmit } = require("../controllers/toDoController");
+const { newToDoSubmit, removeSubmit, completeSubmit } = require("../controllers/toDoController");
 const verifyUser = require("../middleware/verifyUser");
 
 router.get("/", verifyUser, homeRender);
 router.post("/new", verifyUser, newToDoSubmit);
 router.get("/remove/:id", verifyUser, removeSubmit);
+router.get("/complete/:id", verifyUser, completeSubmit);
 router.get("/logout", logoutRender);
 
 module.exports = router;
